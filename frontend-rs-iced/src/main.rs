f8131613ee27b4c4d7e783263c9fd5d209422b66
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2025-07-27 14:53:02 +0530

    Chat input, Time and AM/PM, part-2

--- frontend-rs-iced/src/main.rs
+++ frontend-rs-iced/src/main.rs
@@ -9,6 +9,7 @@ pub enum Message {
     Login,
     Chat,
     ChatInput(String),
+    ChatSubmit,
     NameUpdate(String),
     PassUpdate(String),
 }
@@ -64,9 +65,9 @@ impl Main {
     #[allow(unused)]
     pub fn new() -> Self {
         Self {
-            name: "jaipal".to_string(),
+            name: String::new(),
             pass: String::new(),
-            logged_in: true,
+            logged_in: false,
             chats: load_msg(),
             input: String::new()
         }
@@ -75,17 +76,39 @@ impl Main {
         match page {
             Message::NameUpdate(val) => self.name = val,
             Message::PassUpdate(val) => self.pass = val,
-            Message::Chat => self.logged_in = true,
+            Message::Chat => {
+                if self.name.trim().is_empty() { return; }
+                if self.pass.trim().is_empty() { return; }
+                self.logged_in = true
+            },
             Message::Login => self.logged_in = false,
-            Message::ChatInput(input) => self.input = input
+            Message::ChatInput(input) => self.input = input,
+            Message::ChatSubmit => {
+                if self.input.trim().is_empty() {
+                    return;
+                }
+                let fulltime = chrono::offset::Local::now().to_rfc3339();
+                let time_str = fulltime.split("T").last().unwrap();
+                let time_iter = time_str.splitn(2, ":").filter(|val| val.parse::<u8>().is_ok());
+                let time = time_iter.map(|el| el.parse::<u8>().unwrap()).collect::<Vec<u8>>();
+                self.chats.push(Msg {
+                    author: self.name.clone(),
+                    msg: self.input.clone(),
+                    timestamp: Time {
+                        hr: *time.first().unwrap(),
+                        min: *time.last().unwrap()
+                    }
+                });
+                self.input = String::new();
+            }
         }
     }
     fn user_msg<'a>(&self, last_username: String, el: Msg) -> UserMsg<'a> {
-        if last_username != el.author {
+        if last_username.to_lowercase() != el.author.to_lowercase() {
             let col = column!(
                 text(
-                    if el.author != self.name {
-                        capitalize(el.author.clone())
+                    if el.author.clone().to_uppercase() != self.name.to_uppercase() {
+                        capitalize(el.author.clone().to_lowercase())
                     } else {
                         "You".to_string()
                     }
@@ -94,12 +117,17 @@ impl Main {
                         color: Some(iced::color!(0x94b7f7)),
                     }
                 })
-                    .align_x(if el.author == self.name { Alignment::End } else { Alignment::Start })
-                    .size(15),
+                .align_x(if el.author.clone().to_uppercase() == self.name.to_uppercase() { Alignment::End } else { Alignment::Start })
+                .size(15),
                 container(
                     column!(
                         text(el.msg.clone()).size(20),
-                        text(format!("{}:{}", el.timestamp.hr, el.timestamp.min)).size(10)
+                        text(format!("{}:{}{}", 
+                                if el.timestamp.hr > 12 { el.timestamp.hr - 12 } else {el.timestamp.hr},
+                                el.timestamp.min,
+                                if el.timestamp.hr >= 12 {"PM"} else {"AM"}
+                            )
+                        ).size(10)
                     )
                     .padding(iced::Padding::from([10, 30]))
                 )
@@ -117,7 +145,7 @@ impl Main {
                 })
                 .width(300)
             )
-            .align_x(if el.author == self.name { Alignment::End } else { Alignment::Start })
+            .align_x(if el.author.clone().to_uppercase() == self.name.to_uppercase() { Alignment::End } else { Alignment::Start })
             .spacing(10);
             UserMsg {
                 col, name: el.author
@@ -127,7 +155,12 @@ impl Main {
                 container(
                     column!(
                         text(el.msg.clone()).size(20),
-                        text(format!("{}:{}", el.timestamp.hr, el.timestamp.min)).size(10)
+                        text(format!("{}:{}{}", 
+                                if el.timestamp.hr > 12 { el.timestamp.hr - 12 } else {el.timestamp.hr},
+                                el.timestamp.min,
+                                if el.timestamp.hr >= 12 {"PM"} else {"AM"}
+                            )
+                        ).size(10)
                     )
                     .padding(iced::Padding::from([10, 30]))
                 )
@@ -145,7 +178,7 @@ impl Main {
                 })
                 .width(300)
             )
-            .align_x(if el.author == self.name { Alignment::End } else { Alignment::Start })
+            .align_x(if el.author.clone().to_uppercase() == self.name.to_uppercase() { Alignment::End } else { Alignment::Start })
             .spacing(10);
 
             UserMsg {
@@ -179,7 +212,7 @@ impl Main {
                         last_username = out.name;
                         container(out.col).padding(iced::Padding::from([0, 30]))
                             .width(2000)
-                            .align_x(if self.name == el.author { Alignment::End } else { Alignment::Start })
+                            .align_x(if self.name.to_uppercase() == el.author.clone().to_uppercase() { Alignment::End } else { Alignment::Start })
                             .into()
                     })
                 )
@@ -188,7 +221,7 @@ impl Main {
                 .height(850),
                 row!(
                     text_input("Hello World", &self.input).on_input(Message::ChatInput),
-                    button("send")
+                    button("send").on_press(Message::ChatSubmit)
                 ).padding(iced::Padding::from([30, 50]))
             )
         }
